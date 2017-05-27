efb46f77eeb26e4de3c0350c985ac069b211b31a
vine-nam <wldkdkdk7@gmail.com>
2017-05-27 22:10:49 +0900

    delete not used code
    delete not used code

--- script.js
+++ script.js
@@ -1,43 +1,6 @@
 $(document).ready(function () {
     'use strict';
     
-    var ht = $(window).height();
-    //$("section").height(ht);
-    $(window).on("resize", function () {
-        
-    });
-    
-    $(window).on("scroll", function (e) {
-        var ht = $(window).height();
-        var scroll = $(window).scrollTop();
-        
-        //teaser volume
-        /*
-        var vid = $("video")[0];
-        if(scroll<=1000) {
-            vid.volume = 1-scroll/1000;
-        } else if (scroll>1000){
-            vid.volume = 0;
-        } */
-
-        //scoll
-        /*
-        if(scroll >= (ht*1)/3 && scroll < (ht*2)/3) {
-            $("#synopsis").css({"opacity": "1"});
-        }
-        if(scroll >= (ht*2)/3 && scroll < (ht*3)/3) {
-            $("#character").css({"opacity": "1"});
-        }*/
-        
-        //$("h1").text(scroll+", "+ht);
-        //$("#head").css({"top": scroll});
-        //$("#synopsis").css({"top": -scroll});
-        //$("#character").css({"top": -scroll});
-        //$("#photo").css({"top": -scroll});
-        //$("#video").css({"top": -scroll});
-        //$("#ticket").css({"top": -scroll});
-    
-    });
     //nav controll
     $("header li").on("click", function (e) {
         e.preventDefault();  
@@ -102,29 +65,4 @@ $(document).ready(function () {
        });
     });
 
-    //span
-    $("#character span").on("mouseover", function (e) {
-
-
-    });
-
-    //
-    $("iframe").click(function () {
-        $(this).css({
-            "height": "700px"
-        });
-    });
-    /*
-    $("iframe").hover(
-        function() {
-            $(this).css ({
-                "height": "700px"
-            });
-        },function() {
-            $(this).css ({
-                "height": "100%"
-            });
-        }
-    );
-    */
 });
