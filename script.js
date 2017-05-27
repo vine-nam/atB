3ced1b5f2001d197c5fb27ea9ca86569f6202077
vine-nam <wldkdkdk7@gmail.com>
2017-05-28 00:59:39 +0900

    photo img click
    img show and hide(close)

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ $(document).ready(function () {
         for (i = 1; i <= index; i = i + 1) {
             sumSectionHt += sectionHt.eq(i).outerHeight();
         }
-        $("html, body").stop().animate({"scrollTop": sumSectionHt}, 800);
+        $("html, body").stop().animate({"scrollTop": sumSectionHt}, 700);
 
     });
 
@@ -25,17 +25,37 @@ $(document).ready(function () {
     var r = 0,
         click1 = '',
         click2 = '';
-    $("#character img").on("mouseout", function() {
+    $("#character img").on("mouseout", function () {
         click2 = $(this).next().text();
         click1 = $(this).attr("alt");
         $(this).next().text(click1);
         $(this).attr("alt", click2);
     });
-    $("#character img").on("mouseover", function() {
+    $("#character img").on("mouseover", function () {
         click2 = $(this).next().text();
         click1 = $(this).attr("alt");
         $(this).next().text(click1);
         $(this).attr("alt", click2);
+    });
+    
+    //photo img click
+    $("#photo img").on("click", function () {
+        var imgScr = $(this).attr("src");
+        $("#target").attr("src", imgScr);
+        if (!$(this).hasClass("typeB")) {
+            $("#fullScreen div").attr("class", "fitA");
+        } else {
+            $("#fullScreen div").attr("class", "fitB");
+        }
+        $("#fullScreen").attr("class", "show");
+    });
+    $(".close").on("click", function () {
+        $("#fullScreen").attr("class", "hide");
+    });
+    $("#fullScreen").on("click", function (e) {
+        if (!$(e.target).hasClass("area")) {
+            $("#fullScreen").attr("class", "hide");
+        }
     });
 
 });
